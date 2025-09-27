6e2cc113ca692413c4f68f748de6efda4ab16c47
Mishuba <mishuba14@gmail.com>
2025-09-27 17:06:29 -0400

    workk

--- JS/Functions.js
+++ JS/Functions.js
@@ -1,3 +1,6 @@
+import { HomepageUpdates, FirstGame, letsDoIt } from "./sprite.js";
+
+let TsunamiWeather = document.getElementById("TFweather");
 //Get browser info
 async function getBrowserType() {
     const userAgent = navigator.userAgent;
@@ -49,8 +52,195 @@ export function loadBrowserSpecificScript() {
     }
 
     document.head.appendChild(script);
-} 
+}
 
+function checkIframeOrigin(event, source) {
+    console.log("the iframe origin is " + event.origin);
+    switch (event.origin) {
+        case "https://www.tsunamiflow.club":
+        case "https://tsunamiflow.club":
+            switch (TFiframe.src) {
+                case "homepage.php":
+                case "https://tsunamiflowclub/homepage.php":
+                case "https://www.tsunamiflowclub/homepage.php":
+                    console.log("iframe message received from the homepage");
+                    console.log("The event type is " + event.data.type);
+                    if (event.data.type === "Website Updates") {
+                        console.log("creating the json to send to the iframe");
+                        let HomePageJson = {
+                            type: "start_updates",
+                            info: HomepageUpdates.toJSON(),
+                            message: "Starting the game",
+                            username: "Mishuba",
+                            error: "Nothing as of now"
+                        };
+                        console.log("sending the iframe message");
+                        TFiframe.contentWindow.postMessage(HomePageJson, "https://www.tsunamiflow.club/homepage.php");
+                    } else {
+
+                    }
+                    console.log("the end of that");
+                    break;
+                case "roster.php":
+                case "https://tsunamiflowclub/roster.php":
+                case "https://www.tsunamiflowclub/roster.php":
+                    console.log("The iframe is from the roster page");
+                    let RosterJson = {
+                        type: "roster",
+                        info: "idk roster",
+                        message: "this is the roster message",
+                        username: "Mishuba",
+                        error: "ok no errors for the roster right now"
+                    };
+                    TFiframe.contentWindow.postMessage(RosterJson, "https://www.tsunamiflow.club/roster.php");
+                    break;
+                case "news.php":
+                case "https://tsunamiflowclub/news.php":
+                case "https://www.tsunamiflowclub/news.php":
+                    console.log("The iframe is from the news page");
+                    let NewsJson = {
+                        type: "live",
+                        info: "live stream",
+                        message: "this is the live stream message",
+                        username: "Mishuba",
+                        error: "ok no errors for community right now"
+                    }
+                    TFiframe.contentWindow.postMessage(NewsJson, "https://www.tsunamiflow.club/news.php");
+                    break;
+                case "Competitions.php":
+                case "https://tsunamiflowclub/Competitions.php":
+                case "https://www.tsunamiflowclub/Competitions.php":
+                    console.log("iframe competition message received");
+                    let CompetitionJson;
+                    console.log("checking the data type of the competitions iframe which is " + event.data.type);
+                    if (event.data.type === "start_game") {
+                        console.log("sending the game data over now");
+                        CompetitionJson = {
+                            type: "game_begin",
+                            info: FirstGame.toJSON(),
+                            message: "start the first game once the page opens.",
+                            username: "Mishuba",
+                            error: "Nothing for competitions right now"
+                        };
+
+                        TFiframe.contentWindow.postMessage(CompetitionJson, "https://www.tsunamiflow.club/Competitions.php");
+                    } else if (ev.data.type === "game") {
+                        CompetitionJson = {
+                            type: "game",
+                            info: FirstGame.toJSON(),
+                            message: "this will be the first game message i send from outside of the game for some reason and to the game maybe",
+                            username: "Mishuba",
+                            error: "No errors as of now."
+                        };
+
+                        TFiframe.contentWindow.postMessage(CompetitionJson, "https://www.tsunamiflow.club/Competitions.php");
+                    } else {
+
+                    }
+                    break;
+                case "TFnetwork.php":
+                case "https://tsunamiflow.club/TFnetwork.php":
+                case "https://www.tsunamiflow.club/TFnetwork.php":
+                    console.log("The iframe is from the TFnetwork page");
+                    let TfNetworkJson = {
+                        type: "network",
+                        info: "idk network",
+                        message: "this is the network message",
+                        username: "Mishuba",
+                        error: "ok no errors for network right now"
+                    };
+                    TFiframe.contentWindow.postMessage(TfNetworkJson, "https://www.tsunamiflow.club/TFnetwork.php");
+                    break;
+                case "Community.php":
+                case "https://tsunamiflow.club/Community.php":
+                case "https://www.tsunamiflow.club/Community.php":
+                    console.log("iframe commmunity message received");
+                    let CommunityJson = {
+                        type: "community",
+                        info: "idk community",
+                        message: "this is the community message",
+                        username: "Mishuba",
+                        error: "ok no errors for community right now"
+                    };
+
+                    TFiframe.contentWindow.postMessage(CommunityJson, "https://www.tsunamiflow.club/Community.php");
+                    break;
+                default:
+                    console.log("The iframe is from the default page");
+
+                    break;
+            }
+            break;
+        case "https://world.tsunamiflow.club":
+
+            break;
+        case "https.js.stripe.com":
+
+            break;
+        default:
+
+            break;
+    }
+}
+export async function DoTheThingMan(source) {
+    console.log("starting the main page navigation system.");
+    console.log("checking the iframe source");
+
+    console.log("creating and using the add event listener");
+    switch (source) {
+        case "homepage.php":
+        case "https://tsunamiflowclub/homepage.php":
+        case "https://www.tsunamiflowclub/homepage.php":
+            console.log("The iframe is from the homepage");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        case "roster.php":
+        case "https://tsunamiflowclub/roster.php":
+        case "https://www.tsunamiflowclub/roster.php":
+            console.log("The iframe is from the roster page");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        case "news.php":
+        case "https://tsunamiflowclub/news.php":
+        case "https://www.tsunamiflowclub/news.php":
+            console.log("The iframe is from the news page");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        case "Competitions.php":
+        case "https://tsunamiflowclub/Competitions.php":
+        case "https://www.tsunamiflowclub/Competitions.php":
+            console.log("The iframe is from the competitions page");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        case "TFnetwork.php":
+        case "https://tsunamiflow.club/TFnetwork.php":
+        case "https://www.tsunamiflow.club/TFnetwork.php":
+            console.log("The iframe is from the Tsunami Flow Network");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        case "Community.php":
+        case "https://tsunamiflow.club/Community.php":
+        case "https://www.tsunamiflow.club/Community.php":
+            console.log("The iframe is from the community page");
+            window.addEventListener("message", async (event) => {
+                checkIframeOrigin(event, source);
+            });
+            break;
+        default:
+            console.log(`Some outside source is trying to send my homepage a message. the origin is ${tfEvent.origin}`)
+            break;
+    }
+};
 // Game Mechanics
 /* 
 Air Division 
--- JS/tfMain.js
+++ JS/tfMain.js
@@ -1,14 +1,12 @@
 import { MyNewTFTime, TfUserLanguage, TfUserAgentInfo, TfOnlineStatus } from "./Variables.js";
 import { navButtons } from "./Objects.js";
-import { RadioTimes, WordTimes, DefaultPlaylist } from "./Arrays.js";
+import { RadioTimes, WordTimes } from "./Arrays.js";
 //import { } from "./Classes.js";
 import { WordOfTheDay } from "./Words.js";
 import { NewsTimer } from "./News.js";
-import { HomepageUpdates, FirstGame, letsDoIt } from "./sprite.js";
 import { TfMusic } from "./Audio.js";
 import { Weather } from "./Classes.js";
-
-let TsunamiWeather = document.getElementById("TFweather");
+import { DoTheThingMan } from "./Functions.js";
 
 let TsunamiRadio = document.getElementById("TFradioPlayer");
 let RadioTitle = document.getElementById("TfRadioStuff");
@@ -212,175 +210,16 @@ if (typeof (Worker) !== "undefined") {
 //Tsunami Thoughts 
 document.getElementById("TFthoughtsNow").addEventListener("submit", TsunamiThoughts => {
     TsunamiThoughts.preventDefault();
-    let tfUserThot = document.getElementById("TFthought");
+    //let tfUserThot = document.getElementById("TFthought");
     //TfPostThot(tfUserThot);
 });
 //Tsunami Thoughts Ends
-
-
-async function DoTheThingMan() {
-    console.log("checking the iframe source");
-    if (TFiframe.src === "https://www.tsunamiflow.club/homepage.php" || TFiframe.src === "https://tsunamiflow.club/homepage.php" || TFiframe.src === "homepage.php") {
-        console.log("creating and using the add event listener");
-
-        window.addEventListener("message", async (ev) => {
-            console.log("iframe message received from the homepage");
-
-            console.log("the iframe origin is " + ev.origin);
-
-            if (ev.origin === "https://www.tsunamiflow.club" || ev.origin === "https://tsunamiflow.club") {
-                console.log("The event type is " + ev.data.type);
-                if (ev.data.type === "Website Updates") {
-                    console.l("creating the jsong to send to the iframe");
-                    let HomePageJson = {
-                        type: "start_updates",
-                        info: HomepageUpdates.toJSON(),
-                        message: "Starting the game",
-                        username: "Mishuba",
-                        error: "Nothing as of now"
-                    };
-                    console.log("sending the iframe message");
-                    TFiframe.contentWindow.postMessage(HomePageJson, "https://www.tsunamiflow.club/homepage.php");
-                }
-                console.log("the end of that");
-            } else if (ev.origin === "https://world.tsunamiflow.club") {
-                console.log("I shouldn't be getting stuff from here");
-            } else if (ev.origin === "https.js.stripe.com") {
-                // console.log(ev.data)
-            } else {
-                return console.log(`Some outside source is trying to send my homepage a message. the origin is ${ev.origin}`);
-            }
-        });
-    } else if (TFiframe.src === "https://www.tsunamiflow.club/Community.php" || "https://tsunamiflow.club/Community.php" || "Community.php") {
-        window.addEventListener("message", async (ev) => {
-            console.log("iframe commmunity message received");
-            console.log(ev.origin);
-
-            if (ev.origin === "https://www.tsunamiflow.club" || ev.origin === "https://tsunamiflow.club" || ev.origin === "https://world.tsunamiflow.club") {
-                if (ev.data.type === "community") {
-                    let CommunityJson = {
-                        type: "community",
-                        info: "idk community",
-                        message: "this is the community message",
-                        username: "Mishuba",
-                        error: "ok no errors for community right now"
-                    };
-
-                    TFiframe.contentWindow.postMessage(CommunityJson, "https://www.tsunamiflow.club/Community.php");
-
-                    console.log(CommunityJson);
-                } else {
-                    if (ev.data.error) {
-                        let tfError = ev.data.error;
-                        let CommunityJson = {
-                            type: "community",
-                            info: "idk community",
-                            message: "this is the community message",
-                            username: "Mishuba",
-                            error: tfError
-                        };
-                        TFiframe.contentWindow.postMessage(CommunityJson, "https://www.tsunamiflow.club/Community.php");
-                    } else {
-                        let tfError = "nothing";
-                        console.log("sending the game data over now");
-                        CompetitionJson = {
-                            type: "game_begin",
-                            info: FirstGame.toJSON(),
-                            message: "start the first game once the page opens.",
-                            username: "Mishuba",
-                            error: "Nothing for competitions right now"
-                        };
-
-                        TFiframe.contentWindow.postMessage(CompetitionJson, "https://www.tsunamiflow.club/Competitions.php");
-
-                        console.log("the competitions iframe should have received the message");
-                    }
-                }
-            } else if (ev.origin === "https.js.stripe.com") {
-                //console.log(ev.data);
-            } else {
-                return console.log(`some outside source is trying to send my homepage a message. The origin is ${ev.origin}`);
-            }
-        });
-    } else if (TFiframe.src === "https://www.tsunamiflow.club/news.php" || "https://tsunamiflow.club/news.php" || "news.php") {
-        let NewsJson = {
-
-        };
-    } else if (TFiframe.src === "https://www.tsunamiflow.club/Competitions.php" || "https://tsunamiflow.club/Competitions.php" || "Competitions.php") {
-        console.log("creating the event listener for the competitions iframe");
-        window.addEventListener("message", async (ev) => {
-            console.log("iframe competition message received");
-            console.log(ev.origin);
-            let CompetitionJson;
-
-            console.log("checking the origin from the competitions iframe");
-            if (ev.origin === "https://www.tsunamiflow.club" || ev.origin === "https://tsunamiflow.club" || ev.origin === "https://world.tsunamiflow.club") {
-                console.log("checking the data type of the competitions iframe which is " + ev.data.type);
-                if (ev.data.type === "start_game") {
-                    console.log("sending the game data over now");
-                    CompetitionJson = {
-                        type: "game_begin",
-                        info: FirstGame.toJSON(),
-                        message: "start the first game once the page opens.",
-                        username: "Mishuba",
-                        error: "Nothing for competitions right now"
-                    };
-
-                    TFiframe.contentWindow.postMessage(CompetitionJson, "https://www.tsunamiflow.club/Competitions.php");
-
-                    console.log("the competitions iframe should have received the message");
-                } else if (ev.data.type === "game") {
-                    CompetitionJson = {
-                        type: "game",
-                        info: FirstGame.toJSON(),
-                        message: "this will be the first game message i send from outside of the game for some reason and to the game maybe",
-                        username: "Mishuba",
-                        error: "No errors as of now."
-                    }
-                } else {
-
-                }
-            } else if (ev.origin === "https.js.stripe.com") {
-                // console.log(ev.data)
-            } else {
-
-            }
-        });
-    } else if (TFiframe.src === "https://www.tsunamiflow.club/roster.php" || "https://tsunamiflow.club/roster.php" || "roster.php") {
-        let RosterJson = {
-            type: "roster",
-            info: "idk roster",
-            message: "this is the roster message",
-            username: "Mishuba",
-            error: "ok no errors for the roster right now"
-        };
-    } else if (TFiframe.src === "https://www.tsunamiflow.club/TFnetwork.php" || "https://tsunamiflow.club/TFnetwork.php" || "TFnetwork.php") {
-        let TfNetworkJson = {
-            type: "network",
-            info: "idk network",
-            message: "this is the network message",
-            username: "Mishuba",
-            error: "ok no errors for network right now"
-        };
-    } else {
-        let TfBasicJson = {
-            type: "main",
-            info: "final else option",
-            message: "this is the fallback and default option if nothing is working",
-            username: "Mishuba",
-            error: "ok no errors for main stuff right now"
-        };
-        console.log("something is wrong with the javascript iframe navigation system.");
-        HomepageUpdates.start();
-    }
-};
-//Pages Ended
 
 //Nav Begins
 for (const [key, button] of Object.entries(navButtons)) {
     button.addEventListener("click", () => {
         TFiframe.src = `${key}.php`;
-        DoTheThingMan();
+        DoTheThingMan(TFiframe.src);
     });
 };
 //Nav Ended
