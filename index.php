0f79658e950a4e433052bc47238be650de35da22
Mishuba <mishuba14@gmail.com>
2025-10-18 16:09:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ include "server.php";
                     <input type="hidden" name="cmd" value="_s-xclick" />
                     <input type="hidden" name="hosted_button_id" value="3LQGL334FUKLW" />
                     <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-                    <ilt="Please donate." border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                    <img="Please donate." border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
                 </form>
             </p1>
         </aside>
