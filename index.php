786aa46d7b834722985cc9240741e6617bf1c579
Mishuba <mishuba14@gmail.com>
2025-10-20 18:27:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,6 +47,11 @@ include "server.php";
     <link rel="stylesheet" href="./MyStyle/tfMain.css" />
 
     <!-- Misc -->
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#000000">
+        <meta name="mobile-web-app-capable" content="yes">
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png">
+
     <!--meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="theme-color" content="#ffffff"-->
 </head>
