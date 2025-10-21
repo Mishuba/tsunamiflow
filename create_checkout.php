bfb5a23e2ea540eac8dd3a039c6d33c83569098c
Mishuba <mishuba14@gmail.com>
2025-10-20 21:17:58 -0400

    Update create_checkout.php

--- create_checkout.php
+++ create_checkout.php
@@ -8,40 +8,37 @@ use Stripe\Checkout\Session;
 
 Stripe::setApiKey(TfStripeSecretKey);
 
-if (empty($_SESSION['cart'])) {
-    die('Cart is empty');
-}
+if (empty($_SESSION['cart'])) die('Cart is empty');
 
-// Map cart items for Stripe
-$line_items = [];
-foreach ($_SESSION['cart'] as $item) {
-    $line_items[] = [
-        'price_data' => [
-            'currency' => 'usd',
-            'product_data' => [
-                'name' => 'Product '.$item['product_id'],
-            ],
-            'unit_amount' => intval($item['price'] * 100), // Stripe wants cents
+$line_items=[];
+foreach ($_SESSION['cart'] as $item){
+    $line_items[]=[
+        'price_data'=>[
+            'currency'=>'usd',
+            'product_data'=>['name'=>'Product '.$item['product_id']],
+            'unit_amount'=>intval($item['price']*100)
         ],
-        'quantity' => $item['quantity'],
+        'quantity'=>$item['quantity']
     ];
 }
 
-// Create checkout session
-$session = \Stripe\Checkout\Session::create([
-    'payment_method_types' => ['card'],
-    'line_items' => $line_items,
-    'mode' => 'payment',
-    'success_url' => 'https://yourdomain.com/success.php?session_id={CHECKOUT_SESSION_ID}',
-    'cancel_url' => 'https://yourdomain.com/cancel.php',
-    'customer_email' => $_POST['email'],
-    'metadata' => [
-        'cart' => json_encode($_SESSION['cart']),
-        'name' => $_POST['name'],
-        'address1' => $_POST['address1'],
-        'city' => $_POST['city'],
-        'state' => $_POST['state'],
-        'country' => $_POST['country'],
-        'zip' => $_POST['zip']
+$session = Session::create([
+    'payment_method_types'=>['card'],
+    'line_items'=>$line_items,
+    'mode'=>'payment',
+    'success_url'=>'https://yourdomain.com/success.php?session_id={CHECKOUT_SESSION_ID}',
+    'cancel_url'=>'https://yourdomain.com/cancel.php',
+    'customer_email'=>$_POST['email'],
+    'metadata'=>[
+        'cart'=>json_encode($_SESSION['cart']),
+        'name'=>$_POST['name'],
+        'address1'=>$_POST['address1'],
+        'city'=>$_POST['city'],
+        'state'=>$_POST['state'],
+        'country'=>$_POST['country'],
+        'zip'=>$_POST['zip']
     ]
 ]);
+
+header("Location: ".$session->url);
+exit;
