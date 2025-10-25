01f0fe0c9e510934b186e345fafe7bd68d0d47e4
Mishuba <mishuba14@gmail.com>
2025-10-24 22:58:45 -0400

    Update server.php

--- server.php
+++ server.php
@@ -4,8 +4,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once __DIR__ . "/stripestuff/vendor/autoload.php";
-
 echo "Autoload loaded successfully!";
 
 session_start();
