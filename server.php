dabce77db5adb9d484bf6b5dec9063c67e6ae002
Mishuba <mishuba14@gmail.com>
2025-10-24 23:03:30 -0400

    Update server.php

--- server.php
+++ server.php
@@ -3,8 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-echo "Autoload loaded successfully!";
-
 session_start();
 require_once "config.php";
 //require_once "functions.php";
