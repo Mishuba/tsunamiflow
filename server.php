5764a77de041fbba89486bca19ba70ad6a60bc08
Mishuba <mishuba14@gmail.com>
2025-10-24 23:16:17 -0400

    Update server.php

--- server.php
+++ server.php
@@ -7,8 +7,7 @@ session_start();
 require_once "config.php";
 require_once "functions.php";
 require_once __DIR__ . "/stripestuff/vendor/autoload.php";
-//use Stripe\StripeClient;
-/*
+use Stripe\StripeClient;
 
 // ----------------------------
 // Helper Functions
@@ -201,4 +200,3 @@ if (!empty($s->url)) {
     respond(['error' => $e->getMessage()], 500);
 }
 
-*/
