4f3265ee6ff8346e18ab03b109cf4cb7ad51d0a2
Mishuba <mishuba14@gmail.com>
2025-11-06 00:11:12 -0500

    Update tfMain.js

--- JS/tfMain.js
+++ JS/tfMain.js
@@ -363,3 +363,9 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTotals();
 });
 */
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./../service-worker.js')
+    .then(reg => console.log('Service Worker registered:', reg))
+    .catch(err => console.error('Service Worker registration failed:', err));
+  }
