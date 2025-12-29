b9ca76d4be4d0743525a0f6a139ff1d0897abc1c
Mishuba <mishuba14@gmail.com>
2025-12-29 17:53:05 -0500

    Create webcam.js

---
+++ JS/webcam.js
@@ -0,0 +1,61 @@
+export class TfWebcam {
+    constructor({
+        video = true,
+        audio = false,
+        constraints = {},
+        onReady = null
+    } = {}) {
+        this.stream = null;
+        this.videoTrack = null;
+        this.audioTrack = null;
+
+        this.constraints = {
+            video,
+            audio,
+            ...constraints
+        };
+
+        this.onReady = onReady;
+    }
+
+    async start() {
+        this.stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+
+        this.videoTrack = this.stream.getVideoTracks()[0] || null;
+        this.audioTrack = this.stream.getAudioTracks()[0] || null;
+
+        if (this.onReady) this.onReady(this.stream);
+        return this.stream;
+    }
+
+    stop() {
+        if (!this.stream) return;
+
+        this.stream.getTracks().forEach(t => t.stop());
+        this.stream = null;
+        this.videoTrack = null;
+        this.audioTrack = null;
+    }
+
+    attach(videoElement) {
+        if (!this.stream) throw new Error("Webcam not started");
+        videoElement.srcObject = this.stream;
+    }
+
+    replaceTrack(newTrack) {
+        if (!this.stream) return;
+        const senderTrack = this.stream.getVideoTracks()[0];
+        if (senderTrack) senderTrack.stop();
+        this.stream.removeTrack(senderTrack);
+        this.stream.addTrack(newTrack);
+    }
+
+    toJson() {
+        return {
+            active: !!this.stream,
+            video: !!this.videoTrack,
+            audio: !!this.audioTrack,
+            constraints: this.constraints
+        };
+    }
+}
