3c18e778fcfdfaec13a96b98ed48465cd50222be
Mishuba <mishuba14@gmail.com>
2025-12-30 18:06:40 -0500

    Update recorder.js

--- JS/recorder.js
+++ JS/recorder.js
@@ -50,7 +50,7 @@ buildStream({ canvas }) {
             if (!e.data || e.data.size === 0) return;
 
 const xhr = new XMLHttpRequest();
-  xhr.open("POST", `/ingest.php?key=${STREAM_KEY}`, true);
+  xhr.open("POST", `/TfRTMP.php?key=${STREAM_KEY}`, true);
   xhr.setRequestHeader("Content-Type", "application/octet-stream");
 
   xhr.send(e.data);
