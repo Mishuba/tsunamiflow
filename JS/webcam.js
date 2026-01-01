d9598addf5f01f4e16688af656b9658b519b9c5b
Mishuba <mishuba14@gmail.com>
2025-12-31 21:49:24 -0500

    Update webcam.js

--- JS/webcam.js
+++ JS/webcam.js
@@ -19,19 +19,29 @@ export class TfWebcam {
     }
 
     async start() {
-        this.stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+        if (this.stream) return this.stream;
 
-        this.videoTrack = this.stream.getVideoTracks()[0] || null;
-        this.audioTrack = this.stream.getAudioTracks()[0] || null;
+        try {
+            this.stream = await navigator.mediaDevices.getUserMedia(this.constraints);
 
-        if (this.onReady) this.onReady(this.stream);
-        return this.stream;
+            this.videoTrack = this.stream.getVideoTracks()[0] || null;
+            this.audioTrack = this.stream.getAudioTracks()[0] || null;
+
+            if (this.onReady) this.onReady(this.stream);
+            return this.stream;
+        } catch (err) {
+            console.error("TfWebcam start failed:", err);
+            throw err;
+        }
     }
 
     stop() {
         if (!this.stream) return;
 
-        this.stream.getTracks().forEach(t => t.stop());
+        this.stream.getTracks().forEach(track => {
+            track.stop();
+        });
+
         this.stream = null;
         this.videoTrack = null;
         this.audioTrack = null;
@@ -39,15 +49,35 @@ export class TfWebcam {
 
     attach(videoElement) {
         if (!this.stream) throw new Error("Webcam not started");
-        videoElement.srcObject = this.stream;
+
+        if (videoElement.srcObject !== this.stream) {
+            videoElement.srcObject = this.stream;
+        }
+
+        // Avoid autoplay race conditions
+        if (videoElement.paused) {
+            videoElement.play().catch(() => {});
+        }
     }
 
     replaceTrack(newTrack) {
-        if (!this.stream) return;
-        const senderTrack = this.stream.getVideoTracks()[0];
-        if (senderTrack) senderTrack.stop();
-        this.stream.removeTrack(senderTrack);
+        if (!this.stream || !newTrack) return;
+
+        const kind = newTrack.kind; // "video" or "audio"
+        const oldTrack =
+            kind === "video"
+                ? this.stream.getVideoTracks()[0]
+                : this.stream.getAudioTracks()[0];
+
+        if (oldTrack) {
+            this.stream.removeTrack(oldTrack);
+            oldTrack.stop();
+        }
+
         this.stream.addTrack(newTrack);
+
+        if (kind === "video") this.videoTrack = newTrack;
+        if (kind === "audio") this.audioTrack = newTrack;
     }
 
     toJson() {
