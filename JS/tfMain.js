134b5df8efa2891052b2aeaabd8d86de3632cdf1
Mishuba <mishuba14@gmail.com>
2026-01-02 16:14:56 -0500

    Update tfMain.js

--- JS/tfMain.js
+++ JS/tfMain.js
@@ -84,12 +84,6 @@ Controller.iframe.frame.src = "homepage.html";
 Controller.iframe.MenuSwitch(Controller.iframe.frame);
         Controller.bindNavBar();
 
-    for (const [key, button] of Object.entries(navButtons)) {
-        button.addEventListener("click", () => {
-            Controller.iframe.frame.src = `${key}.html`;
-        });
-    };
-
 Controller.iframe.frame.addEventListener("load", () => {
     console.log("Iframe loaded:", Controller.iframe.frame.src);
 
