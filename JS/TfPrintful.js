512882fee858e5739cd63ccc06931d6740e714ee
Mishuba <mishuba14@gmail.com>
2026-02-06 08:29:34 -0500

    Update TfPrintful.js

--- JS/TfPrintful.js
+++ JS/TfPrintful.js
@@ -6,12 +6,14 @@ xhr.onerror = () => console.error("XHR fetch items failed");
 
 xhr.onload = () => {
 console.log("Printful XHR status:", xhr.status);
-  console.log("Raw response:", xhr.responseText);
+console.log("Raw response:", xhr.responseText);
+
   if (xhr.status !== 200) {
     console.error("Fetch failed:", xhr.responseText);
     return;
-  }
-
+  } else {
   const data = JSON.parse(xhr.responseText);
   console.log("Printful items:", data.items);
 renderPrintfulItems(data.items);
+  }
+};
