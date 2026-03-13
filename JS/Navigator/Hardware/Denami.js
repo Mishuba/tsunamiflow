bee44153a0066c84ce219f254744f976e94308aa
Mishuba <mishuba14@gmail.com>
2026-03-13 12:02:30 -0400

    ok 2

--- JS/Navigator/Hardware/Denami.js
+++ JS/Navigator/Hardware/Denami.js
@@ -1,54 +1,53 @@
 export class Denami {
-  constructor(userAgent = null, platform = null, mobile = null, architecture = null, bitness= null, model = null, version = null, browser = null, version2 = null, webrtc = null, mediaDevices = null, getUserMedia = null, screenShare = null, mediaRecorder = null, mediaSource = null, audioWorklet = null,  speechRecognition = null,
-speechSythesis = null,
-canvas = null,
-webGLRenderingContext = null,
-webGL2RenderingContext = null,
-offScreenCanvas = null,
-webGPU = null,
-wasm = null,
-sharedArrayBuffer = null,
-atomics = null,
-workers = null,
-sharedWorkers = null,
-serviceWorkers = null,
-localStorage = null,
-sessionStorage = null,
-indexedDB = null,
-cacheAPI = null,
-cookies = null,
-webSocket = null,
-eventSource = null,
-fetch = null,
-sendBeacon = null,
-webTransport = null,
-blueTooth = null,
-usb = null,
-serial = null,
-hid = null,
-geoLocation = null,
-vibration = null,
-battery = null,
-touch = null,
-pointerEvents = null,
-gamePad = null,
-clipBoard = null,
-fullScreen = null,
-pictureInPicture = null,
-credentialManager = null,
-webAuth = null,
-permissions = null,
-performanceAPI = null,
-performanceObserver = null,
-requestIdleCallback = null,
-webNN = null,
-webCodecs = null,
- computePressure = null) {
+  constructor(userAgent = null, platform = null, mobile = null, architecture = null, bitness = null, model = null, version = null, browser = null, version2 = null, mediaDevices = null, audioWorklet = null, speechRecognition = null,
+    speechSythesis = null,
+    webGLRenderingContext = null,
+    webGL2RenderingContext = null,
+    offScreenCanvas = null,
+    webGPU = null,
+    wasm = null,
+    sharedArrayBuffer = null,
+    atomics = null,
+    workers = null,
+    sharedWorkers = null,
+    serviceWorkers = null,
+    localStorage = null,
+    sessionStorage = null,
+    indexedDB = null,
+    cacheAPI = null,
+    cookies = null,
+    webSocket = null,
+    eventSource = null,
+    fetch = null,
+    sendBeacon = null,
+    webTransport = null,
+    blueTooth = null,
+    usb = null,
+    serial = null,
+    hid = null,
+    geoLocation = null,
+    vibration = null,
+    battery = null,
+    touch = null,
+    pointerEvents = null,
+    gamePad = null,
+    clipBoard = null,
+    fullScreen = null,
+    pictureInPicture = null,
+    credentialManager = null,
+    webAuth = null,
+    permissions = null,
+    performanceAPI = null,
+    performanceObserver = null,
+    requestIdleCallback = null,
+    webNN = null,
+    webCodecs = null,
+    computePressure = null) {
     this.userAgent = userAgent;
     this.HubyMaxx = platform;
     this.mobile = mobile;
     this.architecture = architecture;
-    this.bitness = bitness; 
+    this.bitness = bitness;
     this.model = model;
     this.version = platformVersion;
     this.browser = browser;
@@ -105,32 +104,32 @@ webCodecs = null,
       return result;
     } else {
       this.userAgent = navigator.userAgent;
-      
+
       const HubyMaxxData = navigator.userAgentData;
-      
+
       this.HubyMaxx = HubyMaxxData.platform || null;
       this.mobile = HubyMaxxData.mobile || false;
-      
-      let DetailDeviceInfo = HubyMaxxData.getHighEntropyValues([
-          "fullVersionList"
-        ]);
-        
-        this.architecture = DetailDeviceInfo.architecture;
-        this.bitness = DetailDeviceInfo.bitness;
-        this.model = DetailDeviceInfo.model;
-        this.version = DetailDeviceInfo.platformVersion;
-        
-        if (DetailDeviceInfo.fullVersionList && DetailDeviceInfo.fullVersionList.length) {
-          let TfMainDevice = DetailDeviceInfo.fullVersionList.find(b =>
-  !b.brand.includes("Not") && !b.brand.includes("Chromium")
-) || high.fullVersionList[0];
 
-          this.browser = TfMainDevice.brand;
-          this.browserVersion = TfMainDevice.version;
-        }
-        
+      let DetailDeviceInfo = HubyMaxxData.getHighEntropyValues([
+        "fullVersionList"
+      ]);
+
+      this.architecture = DetailDeviceInfo.architecture;
+      this.bitness = DetailDeviceInfo.bitness;
+      this.model = DetailDeviceInfo.model;
+      this.version = DetailDeviceInfo.platformVersion;
+
+      if (DetailDeviceInfo.fullVersionList && DetailDeviceInfo.fullVersionList.length) {
+        let TfMainDevice = DetailDeviceInfo.fullVersionList.find(b =>
+          !b.brand.includes("Not") && !b.brand.includes("Chromium")
+        ) || high.fullVersionList[0];
+
+        this.browser = TfMainDevice.brand;
+        this.browserVersion = TfMainDevice.version;
+      }
+
       return;
     }
   }
-  
+
 }
---
+++ JS/namiDraw.js
@@ -0,0 +1,5 @@
+export class NamiDraw {
+    constructor(canvas = null,) {
+
+    }
+}
