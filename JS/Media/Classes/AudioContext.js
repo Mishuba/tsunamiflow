121fc9245725a9907668f0d099e6253bf172395d
Mishuba <mishuba14@gmail.com>
2026-03-13 13:21:38 -0400

    Update AudioContext.js

--- JS/Media/Classes/AudioContext.js
+++ JS/Media/Classes/AudioContext.js
@@ -1,6 +1,20 @@
 export class TsunamiSounds {
-      constructor (context = null) {
-            this.main = context;
-            
+      constructor () {
+            this.context = null;
+            this.processing = null;
+            this.id = null;
       }
+      create() {
+            if(this.context === null) {
+                  this.context = new AudioContext();
+                  this.processing = this.context.baseLatency;
+                  this.output = this.context.outputLatency;
+            } else {
+                  return this.context;
+            }
+      }
+      finish() {
+            this.context.close();
+      }
+      
 }
