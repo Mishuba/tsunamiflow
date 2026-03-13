92e26aab9334e6bb6bcd881d633fb2961d7ef73f
Mishuba <mishuba14@gmail.com>
2026-03-13 19:46:48 -0400

    Update webgl2.js

---
+++ JS/Window/Graphics/Classes/webgl2.js
@@ -0,0 +1,58 @@
+export class TfWebGL2 {
+    constructor(canvas) {
+        if (!canvas) throw new Error("Canvas element is required");
+        this.canvas = canvas;
+        this.gl = null;
+        this.isReady = false;
+    }
+
+    init() {
+        try {
+            this.gl = this.canvas.getContext("webgl2");
+            if (!this.gl) throw new Error("WebGL2 not supported");
+            this.isReady = true;
+            console.log("WebGL2 initialized");
+        } catch (err) {
+            console.error("WebGL2 init failed:", err);
+            this.gl = null;
+        }
+    }
+
+    clear(r = 0, g = 0, b = 0, a = 1) {
+        if (!this.isReady) return;
+        this.gl.clearColor(r, g, b, a);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    }
+
+    resize(width, height) {
+        if (!this.isReady) return;
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.gl.viewport(0, 0, width, height);
+    }
+
+    createShader(type, source) {
+        if (!this.isReady) return null;
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            console.error("Shader compile error:", this.gl.getShaderInfoLog(shader));
+            return null;
+        }
+        return shader;
+    }
+
+    createProgram(vertexShader, fragmentShader) {
+        if (!this.isReady) return null;
+        const program = this.gl.createProgram();
+        this.gl.attachShader(program, vertexShader);
+        this.gl.attachShader(program, fragmentShader);
+        this.gl.linkProgram(program);
+        if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+            console.error("Program link error:", this.gl.getProgramInfoLog(program));
+            return null;
+        }
+        return program;
+    }
+}
