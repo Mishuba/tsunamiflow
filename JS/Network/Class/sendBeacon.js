5442c9ea6e15a6a2ce9e9c0e3cf950e46a2248ba
Mishuba <mishuba14@gmail.com>
2026-03-13 19:42:32 -0400

    Update sendBeacon.js

---
+++ JS/Network/Class/sendBeacon.js
@@ -0,0 +1,21 @@
+export class TfSendBeacon {
+    constructor() {
+        this.listeners = {};
+    }
+
+    send(url, data) {
+        const blob = data instanceof Blob ? data : new Blob([JSON.stringify(data)], { type: "application/json" });
+        const success = navigator.sendBeacon(url, blob);
+        this.emit(success ? "success" : "error", { url, data });
+    }
+
+    emit(event, data) {
+        const listeners = this.listeners[event] || [];
+        listeners.forEach(fn => fn(data));
+    }
+
+    on(event, fn) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(fn);
+    }
+}
