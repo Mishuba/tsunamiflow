98ed758e9761bdad809397b8dd60892b91f1b853
Mishuba <mishuba14@gmail.com>
2026-03-13 23:01:32 -0400

    Update SpeechSynthesis.js

--- JS/TFN/F/Media/Classes/SpeechSynthesis.js
+++ JS/TFN/F/Media/Classes/SpeechSynthesis.js
@@ -1,42 +1,100 @@
 export class TfSpeechSynthesis {
     constructor() {
-        if (!window.speechSynthesis) {
-            console.warn("Speech Synthesis not supported");
-            this.synth = null;
+        this.supported = 'speechSynthesis' in window;
+        this.synth = this.supported ? window.speechSynthesis : null;
+        this.queue = [];
+        this.activeUtterance = null;
+        this.listeners = {};
+
+        if (!this.supported) {
+            console.warn("Speech Synthesis not supported in this browser.");
             return;
         }
-        this.synth = window.speechSynthesis;
-        this.queue = [];
     }
 
+    /* ----------------------------
+       Speak Text
+    -----------------------------*/
     speak(text, options = {}) {
-        if (!this.synth) return;
+        if (!this.supported || !text) return;
 
         const utter = new SpeechSynthesisUtterance(text);
         utter.lang = options.lang || "en-US";
-        utter.pitch = options.pitch || 1;
-        utter.rate = options.rate || 1;
-        utter.volume = options.volume || 1;
+        utter.pitch = options.pitch ?? 1;
+        utter.rate = options.rate ?? 1;
+        utter.volume = options.volume ?? 1;
 
-        if (options.onstart) utter.onstart = options.onstart;
-        if (options.onend) utter.onend = options.onend;
-        if (options.onerror) utter.onerror = options.onerror;
+        // Attach events
+        utter.onstart = () => {
+            this.activeUtterance = utter;
+            this.emit("start", utter.text);
+            if (options.onstart) options.onstart(utter);
+        };
+        utter.onend = () => {
+            this.activeUtterance = null;
+            this.emit("end", utter.text);
+            if (options.onend) options.onend(utter);
+            // Auto-play next in queue
+            if (this.queue.length > 0) this.speak(this.queue.shift());
+        };
+        utter.onerror = (err) => {
+            this.activeUtterance = null;
+            this.emit("error", err);
+            if (options.onerror) options.onerror(err);
+        };
 
-        this.synth.speak(utter);
+        // Queue handling
+        if (this.synth.speaking) {
+            this.queue.push(text);
+        } else {
+            this.synth.speak(utter);
+        }
     }
 
-    cancel() {
-        if (!this.synth) return;
-        this.synth.cancel();
-    }
-
+    /* ----------------------------
+       Pause, Resume, Cancel
+    -----------------------------*/
     pause() {
-        if (!this.synth) return;
+        if (!this.supported || !this.synth.speaking) return;
         this.synth.pause();
+        this.emit("pause");
     }
 
     resume() {
-        if (!this.synth) return;
+        if (!this.supported || !this.synth.paused) return;
         this.synth.resume();
+        this.emit("resume");
+    }
+
+    cancel() {
+        if (!this.supported) return;
+        this.synth.cancel();
+        this.queue = [];
+        this.activeUtterance = null;
+        this.emit("cancel");
+    }
+
+    /* ----------------------------
+       Event System
+    -----------------------------*/
+    on(event, callback) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(callback);
+    }
+
+    emit(event, data) {
+        (this.listeners[event] || []).forEach(cb => cb(data));
+    }
+
+    /* ----------------------------
+       Quick JSON Status
+    -----------------------------*/
+    toJson() {
+        return {
+            supported: this.supported,
+            speaking: this.synth?.speaking || false,
+            paused: this.synth?.paused || false,
+            queueLength: this.queue.length
+        };
     }
 }
