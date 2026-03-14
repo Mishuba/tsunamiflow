73eb38e1810c0585a24b1c3cea3e37a193e8fdc8
Mishuba <mishuba14@gmail.com>
2026-03-13 23:12:25 -0400

    Update Streamer.js

--- JS/TFN/F/Video/Class/Streamer.js
+++ JS/TFN/F/Video/Class/Streamer.js
@@ -1,55 +1,95 @@
-export class TfMediaStream {
-  constructor() {
-    this.canvas = null;
-    this.audioContext = null;
-    this.sourceNode = null;
+export class TsunamiFlowMediaStream {
+    constructor() {
+        this.canvas = null;
+        this.audioContext = null;
+        this.sourceNode = null;
 
-    this.videoStream = null;
-    this.audioStream = null;
-    this.stream = null;
-  }
+        this.videoStream = null;
+        this.audioStream = null;
+        this.stream = null;
 
-  start({
-    canvas,
-    audioContext = null,
-    sourceNode = null,
-    fps = 30
-  }) {
-    if (!canvas) {
-      throw new Error("Canvas is required to create MediaStream");
+        this.listeners = {};
     }
 
-    this.canvas = canvas;
-    this.audioContext = audioContext;
-    this.sourceNode = sourceNode;
+    /* ----------------------------
+       Start the MediaStream
+    ----------------------------*/
+    start({ canvas, audioContext = null, sourceNode = null, fps = 30 }) {
+        if (!canvas) throw new Error("Canvas is required to create MediaStream");
 
-    // ---- VIDEO (canvas is king)
-    this.videoStream = canvas.captureStream(fps);
+        this.canvas = canvas;
+        this.audioContext = audioContext;
+        this.sourceNode = sourceNode;
 
-    const tracks = [...this.videoStream.getVideoTracks()];
+        // Capture video from canvas
+        this.videoStream = canvas.captureStream(fps);
+        const tracks = [...this.videoStream.getVideoTracks()];
 
-    // ---- AUDIO (optional, but correct)
-    if (audioContext && sourceNode) {
-      const dest = audioContext.createMediaStreamDestination();
-      sourceNode.connect(dest);
+        // Capture audio from AudioContext if provided
+        if (audioContext && sourceNode) {
+            const dest = audioContext.createMediaStreamDestination();
+            sourceNode.connect(dest);
+            this.audioStream = dest.stream;
+            tracks.push(...this.audioStream.getAudioTracks());
+        }
 
-      this.audioStream = dest.stream;
-      tracks.push(...this.audioStream.getAudioTracks());
+        // Combine video + audio
+        this.stream = new MediaStream(tracks);
+
+        this.emit("start", this.stream);
+
+        return this.stream;
     }
 
-    // ---- FINAL STREAM
-    this.stream = new MediaStream(tracks);
+    /* ----------------------------
+       Stop all tracks
+    ----------------------------*/
+    stop() {
+        if (this.stream) {
+            this.stream.getTracks().forEach(track => track.stop());
+        }
 
-    return this.stream;
-  }
+        this.videoStream = null;
+        this.audioStream = null;
+        this.stream = null;
 
-  stop() {
-    if (this.stream) {
-      this.stream.getTracks().forEach(t => t.stop());
+        this.emit("stop");
     }
 
-    this.videoStream = null;
-    this.audioStream = null;
-    this.stream = null;
-  }
+    /* ----------------------------
+       Replace canvas dynamically
+    ----------------------------*/
+    replaceCanvas(newCanvas, fps = 30) {
+        if (!newCanvas) throw new Error("New canvas is required");
+        this.canvas = newCanvas;
+        if (this.stream) this.stop();
+        return this.start({ canvas: newCanvas, audioContext: this.audioContext, sourceNode: this.sourceNode, fps });
+    }
+
+    /* ----------------------------
+       Event System
+    ----------------------------*/
+    on(event, callback) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(callback);
+    }
+
+    emit(event, data) {
+        (this.listeners[event] || []).forEach(cb => cb(data));
+    }
+
+    /* ----------------------------
+       Utility: Get stream safely
+    ----------------------------*/
+    getStream() {
+        return this.stream;
+    }
+
+    getVideoTracks() {
+        return this.videoStream ? this.videoStream.getVideoTracks() : [];
+    }
+
+    getAudioTracks() {
+        return this.audioStream ? this.audioStream.getAudioTracks() : [];
+    }
 }
