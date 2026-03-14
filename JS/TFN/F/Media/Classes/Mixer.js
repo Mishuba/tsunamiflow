d1d3704a47fc7f8045fa88109c5efd8f751ea1fe
Mishuba <mishuba14@gmail.com>
2026-03-13 22:56:46 -0400

    Update Mixer.js

--- JS/TFN/F/Media/Classes/Mixer.js
+++ JS/TFN/F/Media/Classes/Mixer.js
@@ -1,51 +1,106 @@
 export class TfAudioMixer {
     constructor(audioContext) {
+        if (!audioContext) throw new Error("AudioContext is required");
         this.ctx = audioContext;
         this.destination = this.ctx.createMediaStreamDestination();
-
-        this.gains = {
-            mic: this.ctx.createGain(),
-            music: this.ctx.createGain(),
-            fx: this.ctx.createGain()
-        };
-
-        this.sources = {};
+        this.gains = {};       // Gain nodes per source
+        this.sources = {};     // Media sources by id
+        this.listeners = {};   // Event system
+        this.idCounter = 0;
     }
 
-    async attachMic() {
+    /* ----------------------------
+       Add Microphone
+    -----------------------------*/
+    async addMic(id = null) {
+        const sourceId = id || `mic-${++this.idCounter}`;
         const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        const micSource = this.ctx.createMediaStreamSource(micStream);
-        micSource.connect(this.gains.mic).connect(this.destination);
-        this.sources.mic = micSource;
-        return micStream;
+        const source = this.ctx.createMediaStreamSource(micStream);
+        const gain = this.ctx.createGain();
+        source.connect(gain).connect(this.destination);
+
+        this.sources[sourceId] = source;
+        this.gains[sourceId] = gain;
+
+        this.emit("sourceAdded", { id: sourceId, type: "mic", source, gain });
+        return { stream: micStream, id: sourceId };
     }
 
-    attachMusic(mediaElement) {
-        const source = this.ctx.createMediaElementSource(mediaElement);
-        source.connect(this.gains.music).connect(this.destination);
-        source.connect(this.ctx.destination); // local monitoring
-        this.sources.music = source;
+    /* ----------------------------
+       Add MediaElement (music / fx)
+    -----------------------------*/
+    addMediaElement(element, id = null, monitor = false) {
+        if (!element) throw new Error("MediaElement is required");
+        const sourceId = id || `element-${++this.idCounter}`;
+        const source = this.ctx.createMediaElementSource(element);
+        const gain = this.ctx.createGain();
+
+        source.connect(gain).connect(this.destination);
+        if (monitor) source.connect(this.ctx.destination); // local playback
+
+        this.sources[sourceId] = source;
+        this.gains[sourceId] = gain;
+
+        this.emit("sourceAdded", { id: sourceId, type: "media", source, gain });
+        return sourceId;
     }
 
-    attachFx(audioElement) {
-        const source = this.ctx.createMediaElementSource(audioElement);
-        source.connect(this.gains.fx).connect(this.destination);
-        source.connect(this.ctx.destination);
-        return source;
+    /* ----------------------------
+       Set gain / volume per source
+    -----------------------------*/
+    setVolume(id, value = 1) {
+        if (this.gains[id]) this.gains[id].gain.value = value;
     }
 
-    setVolume(type, value) {
-        if (this.gains[type]) {
-            this.gains[type].gain.value = value;
+    /* ----------------------------
+       Remove a source
+    -----------------------------*/
+    removeSource(id) {
+        if (this.sources[id]) {
+            this.sources[id].disconnect();
+            this.gains[id].disconnect();
+            delete this.sources[id];
+            delete this.gains[id];
+            this.emit("sourceRemoved", id);
         }
     }
 
+    /* ----------------------------
+       Get mixed MediaStream
+    -----------------------------*/
     getStream() {
         return this.destination.stream;
     }
 
+    /* ----------------------------
+       Reset all sources
+    -----------------------------*/
     reset() {
         Object.values(this.sources).forEach(src => src.disconnect());
         this.sources = {};
+        this.gains = {};
+        this.emit("reset");
+    }
+
+    /* ----------------------------
+       Event system
+    -----------------------------*/
+    on(event, fn) {
+        if (!this.listeners[event]) this.listeners[event] = [];
+        this.listeners[event].push(fn);
+    }
+
+    emit(event, data) {
+        (this.listeners[event] || []).forEach(fn => fn(data));
+    }
+
+    /* ----------------------------
+       Quick status
+    -----------------------------*/
+    toJson() {
+        return {
+            sources: Object.keys(this.sources),
+            destinationConnected: !!this.destination
+        };
     }
 }
