1ca64507a64d2495a32e06238057d37c1c244627
Mishuba <mishuba14@gmail.com>
2026-03-22 21:12:56 -0400

    Update Hardware.js

--- JS/TFN/T/Class/Base/Hardware.js
+++ JS/TFN/T/Class/Base/Hardware.js
@@ -1,23 +1,89 @@
 export class na extend Tsu {
+this.uaData = navigator.userAgentData || null;
+this.LegacyUaData = navigator.userAgent || null;
+
     TsunamiLocation = !!navigator.geolocation;
 
 constructor(options = {}) {
       super(options);
       
+        
+    }
+
+    getBrands() {
+        if (!(!!this.uaData) {
+             return [];
+        } else {
+        return this.uaData.brands || [];
+        }
+    }
+
+    getMobile() {
+        if (!(!!this.uaData) {
+             return null;
+        } else {
+              return this.uaData.mobile;
+        }
+    }
+
+    getPlatform() {
+        if (!(!!this.uaData) {
+             return null;
+        } else {
+              return this.uaData.platform;
+        }
+    }
+
+    async getHighEntropyValues(hints = [
+        "architecture",
+        "bitness",
+        "model",
+        "platformVersion",
+        "uaFullVersion"
+    ]) {
+        if (!(!!this.uaData) {
+             return null;
+        } else {
+              try {
+                  return await this.uaData.getHighEntropyValues(hints);
+              } catch (err) {
+                  console.error("Failed to retrieve high entropy UA values:", err);
+                  return null;
+              }
+        }
+    }
+
+    getFullInfo() {
+        return {
+            supported: this.supported,
+            brands: this.getBrands(),
+            mobile: this.getMobile(),
+            platform: this.getPlatform(),
+            legacyUA: this.LegacyUaData
+        };
     }
 
     getCurrentPosition(success, error, options = {}) {
-        if (!this.supported) return null;
-        navigator.geolocation.getCurrentPosition(success, error, options);
+        if (!TsunamiLocation) {
+             return null;
+        } else {
+return navigator.geolocation.getCurrentPosition(success, error, options);
+       }
     }
 
     watchPosition(success, error, options = {}) {
-        if (!this.supported) return null;
-        return navigator.geolocation.watchPosition(success, error, options);
+        if (!TsunamiLocation) { 
+            return null;
+        } else {
+            return navigator.geolocation.watchPosition(success, error, options);
+        }
     }
 
     clearWatch(id) {
-        if (!this.supported) return;
-        navigator.geolocation.clearWatch(id);
+        if (!TsunamiLocation) else {
+            return;
+        } else {
+           navigator.geolocation.clearWatch(id);
+        }
     }
 }
