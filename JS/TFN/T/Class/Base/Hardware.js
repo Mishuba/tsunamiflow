c8b9a5061a31252bfaf1c0f596c3b7df4499e8ca
Mishuba <mishuba14@gmail.com>
2026-03-22 20:56:21 -0400

    Update Hardware.js

--- JS/TFN/T/Class/Base/Hardware.js
+++ JS/TFN/T/Class/Base/Hardware.js
@@ -1,7 +1,23 @@
 export class na extend Tsu {
+    TsunamiLocation = !!navigator.geolocation;
 
 constructor(options = {}) {
       super(options);
       
-}
+    }
+
+    getCurrentPosition(success, error, options = {}) {
+        if (!this.supported) return null;
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+    watchPosition(success, error, options = {}) {
+        if (!this.supported) return null;
+        return navigator.geolocation.watchPosition(success, error, options);
+    }
+
+    clearWatch(id) {
+        if (!this.supported) return;
+        navigator.geolocation.clearWatch(id);
+    }
 }
