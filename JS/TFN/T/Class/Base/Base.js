37560c095cdf6876f5877cc9c26180b1d6ef64d7
Mishuba <mishuba14@gmail.com>
2026-03-23 18:12:45 -0400

    Update Base.js

--- JS/TFN/T/Class/Base/Base.js
+++ JS/TFN/T/Class/Base/Base.js
@@ -39,6 +39,27 @@ export class Tsu {
             }
         });
     }
+    request(method, url, data = null, headers = {}) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+
+        for (const [key, value] of Object.entries(headers)) {
+            xhr.setRequestHeader(key, value);
+        }
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    this.emit("success", xhr.responseText);
+                } else {
+                    this.emit("error", xhr.statusText);
+                }
+            }
+        };
+
+        xhr.send(data);
+        return xhr;
+    }
     on(id, eventName, preventDefault = false, iframe = null) {
         let el = this.find(id, iframe);
 
