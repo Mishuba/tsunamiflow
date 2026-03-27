36525a0223790a9caec5a46b86e7ce0d8aac4fdf
Mishuba <mishuba14@gmail.com>
2026-03-27 17:41:08 -0400

    yeah

---
+++ JS/TFN/T/Class/Elder/Adult/Video.js
@@ -0,0 +1,10 @@
+export class TfVideo {
+    constructor() {
+
+    }
+    VideoWebCodecs(stream) {
+        const track = stream.getVideoTracks()[0];
+        this.VideoProcessor = new MediaStreamTrackProcessor({ track });
+        this.VideoReader = this.VideoProcessor.readable.getReader();
+    }
+}
---
+++ JS/TFN/T/Class/Elder/Media.js
@@ -0,0 +1,7 @@
+export class M {
+    audio = null;
+    constructor(option = {}) {
+
+    }
+
+}
