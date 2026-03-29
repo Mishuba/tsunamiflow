6efc5be0a1b7c8f207710602f3ac8d48d76f364b
Mishuba <mishuba14@gmail.com>
2026-03-29 05:41:36 -0400

    Update Shared.js

--- JS/TFN/T/Worker/Shared.js
+++ JS/TFN/T/Worker/Shared.js
@@ -1,37 +1,44 @@
-export class SharedWorker extends F {
-    scriptURL = "";
-    sharedWorker = null;
-    port = null;
-    listeners = {};
-    constructor(options = {}) {
-        if (!window.SharedWorker) {
-            console.warn("Shared Workers are not supported in this browser");
-            this.sharedWorker = null;
-            return;
-        } else {
-            if (options.scriptURL) this.scriptURL = options.scriptURL;
+importScripts("F.js"); // or bundle it
+
+const ports = [];
+const core = new F({
+    role: "viewer"
+});
+
+core.setMessageHandler((data) => {
+    ports.forEach(p => p.postMessage({
+        type: "ws_message",
+        data
+    }));
+});
+
+onconnect = (e) => {
+    const port = e.ports[0];
+    ports.push(port);
+
+    port.start();
+
+    port.onmessage = (event) => {
+        const msg = event.data;
+
+        switch (msg.type) {
+            case "connect":
+                core.connectws();
+                break;
+
+            case "disconnect":
+                core.disconnectws();
+                break;
+
+            case "send":
+                core.sendwsJSON(msg.data);
+                break;
+
+            case "binary":
+                core.sendBinaryws(msg.data);
+                break;
         }
-    }
-    start() {
-        if (!this.scriptURL) return;
-        this.sharedWorker = new SharedWorker(this.scriptURL);
-        this.port = this.sharedWorker.port;
+    };
 
-        this.port.onmessage = (event) => this.emit("message", event.data);
-        this.port.start(); // start the port explicitly
-        console.log("Shared Worker started");
-    }
-
-    postMessage(data) {
-        if (!this.port) return;
-        this.port.postMessage(data);
-    }
-
-    close() {
-        if (!this.port) return;
-        this.port.close();
-        this.sharedWorker = null;
-        this.port = null;
-        console.log("Shared Worker closed");
-    }
-}
+    port.postMessage({ type: "ready" });
+};
