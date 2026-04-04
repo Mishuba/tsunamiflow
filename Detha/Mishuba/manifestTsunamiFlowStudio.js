7e68947167edaee93225beb8e70fb4088735c5bb
Mishuba <mishuba14@gmail.com>
2026-04-04 10:52:01 -0400

    Create manifestTsunamiFlowStudio.js

---
+++ Detha/Mishuba/manifestTsunamiFlowStudio.js
@@ -0,0 +1,24 @@
+{
+  "name": "Tsunami Flow Studio",
+  "short_name": "TF Studio",
+  "description": "Tsunami Flow Nation Recording Studio Application",
+  "start_url": "/apps/MusicStudio/",
+  "scope": "/apps/MusicStudio/",
+  "display": "standalone",
+  "display_override": ["window-controls-overlay", "standalone"],
+  "orientation": "landscape",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "/studio/icons/192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/studio/icons/512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
