760671227d50c88321862d0d06162ada703c8e6b
Mishuba <mishuba14@gmail.com>
2026-04-17 18:22:19 -0400

    Update base.js

--- JS/TFN/T/Class/Elder/Adult/Teen/Child/Base/base.js
+++ JS/TFN/T/Class/Elder/Adult/Teen/Child/Base/base.js
@@ -4,4 +4,16 @@ constructor(options = {}) {
 
 }
 
+    async tycadome(id, type, action, meta, state, mode, payload) {
+        return {
+            "id": id, //options.id
+            "type": type, //command
+            "action": action, // video.start
+            "meta": meta,
+            "timestamp": Math.floor(Date.now() / 1000),
+            "state": state,
+            "mode": mode, //"async"
+            "payload": payload // {}
+        };;
+    }
 }
