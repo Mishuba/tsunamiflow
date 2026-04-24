b77622b83ab7e6e0daae5476a8a7d115f8909147
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-04-24 00:17:40 -0400

    Update tfMain.js

--- JS/tfMain.js
+++ JS/tfMain.js
@@ -305,7 +305,7 @@ document.addEventListener("DOMContentLoaded", () => {
 console.log("main js:" + import.meta.url);
 
   Controller.worker = new Worker(
-    new URL("./JS/TFN/T/Worker/WebWorker/kid/MediaWebWorker.js", import.meta.url),
+    new URL("TFN/T/Worker/WebWorker/kid/MediaWebWorker.js", import.meta.url),
     { type: "module" }
   );
 
