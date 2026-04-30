0487409e219a3725ce7641181eef623c295ae3a7
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-04-29 20:20:14 -0400

    Update TfRadio.js

--- JS/TFN/T/Class/Elder/TfRadio.js
+++ JS/TFN/T/Class/Elder/TfRadio.js
@@ -350,7 +350,7 @@ export class TsunamiFlowRadio extends TsunamiFlowAudio {
         canvas: this.visualizatorController,
         //        analyser: this.updateAnalyser(),
         dataArray: this.TfSoundsContextDataArray,
-        bufferLength: this.TfSoundsContextDataArray.length, baseRadius: this.baseRadius,
+        bufferLength: this.TfSoundsContextDataArray.buffer, baseRadius: this.baseRadius,
         particles: this.particles
       }),
       [this.visualizatorController, this.TfSoundsContextDataArray.buffer]);
