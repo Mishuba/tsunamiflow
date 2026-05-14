b27495939560e79a5ecb2f6a97cc69e48c70d94a
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-05-14 15:23:56 -0400

    Update TfNationProcessor.js

--- JS/TFN/T/Class/Elder/Adult/TfNationProcessor.js
+++ JS/TFN/T/Class/Elder/Adult/TfNationProcessor.js
@@ -1,22 +1,45 @@
-class AnalyzerProcessor extends AudioWorkletProcessor {
-  process(inputs) {
+class FFTProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+
+    this.bufferSize = 2048;
+    this.buffer = new Float32Array(this.bufferSize);
+    this.writeIndex = 0;
+  }
+
+  process(inputs, outputs) {
     const input = inputs[0];
-    const channel = input?.[0];
+    if (!input || !input[0]) return true;
 
-    if (!channel) return true;
+    const channel = input[0];
+    const output = outputs[0];
 
-    let sum = 0;
-
-    for (let i = 0; i < channel.length; i++) {
-      sum += channel[i] * channel[i];
+    // 1. pass-through audio (zero distortion graph behavior)
+    if (output && output[0]) {
+      output[0].set(channel);
     }
 
-    const rms = Math.sqrt(sum / channel.length);
+    // 2. collect samples ONLY
+    for (let i = 0; i < channel.length; i++) {
+      this.buffer[this.writeIndex++] = channel[i];
 
-    this.port.postMessage({ rms });
+      if (this.writeIndex >= this.bufferSize) {
+
+        // 🔥 SEND RAW AUDIO SNAPSHOT ONLY
+        // NO FFT HERE
+        this.port.postMessage(
+          this.buffer,
+          [this.buffer.buffer] // transfer ownership = zero copy
+        );
+
+        // recreate buffer (required after transfer)
+        this.buffer = new Float32Array(this.bufferSize);
+        this.writeIndex = 0;
+      }
+    }
 
     return true;
   }
 }
 
-registerProcessor("analyzer", AnalyzerProcessor);
+registerProcessor("fft-processor", FFTProcessor);
