bd906ddf76ae811a0c34549af84610aaa9928032
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-09-08 20:19:36 -0400

    ok12345

--- JS/TFN/maxwell.js
+++ JS/TFN/maxwell.js
@@ -802,7 +802,7 @@ export class maxwell {
 
                     for (const key in sounds) fxSources[key] = this.soundEngine.initAudioContext(this.worker, sounds[key], { type: "video", element: "audio" }, "video");
 
-                    let newplaylistoptions = find("liveplaylist");
+                    let newplaylistoptions = find("liveplaylist", this.iframe.frame);
 
                     if (this.soundEngine.radioSchedule === null) {
                         for (let lilj = 0; lilj < this.soundEngine.DefaultPlaylist.length; lilj++) {
