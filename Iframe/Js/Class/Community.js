c26a87d546e67cda6d744a134347ebd3707a7182
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-09-08 21:12:43 -0400

    ok123456789

--- Iframe/Js/Class/Community.js
+++ Iframe/Js/Class/Community.js
@@ -30,11 +30,81 @@ document.addEventListener("DOMContentLoaded", async () => {
             [VideoOffscreenCanvas]);
         controller.videoEngine.videoElement = document.getElementById("TsunamiFlowVideoStuff");
         controller.newplaylistoptions = document.getElementById("liveplaylist");
+
     } catch (err) {
 
     } finally {
         controller.bindVidSystem();
-        
-    }
 
+        self.onmessage = async (event) => {
+
+        }
+        self.onmessageerror = (err) => {
+            console.error("Error handling message in Community.js:", err);
+        };
+        self.onerror = (err) => {
+            console.error("Error in Community.js:", err);
+        }
+
+        try {
+
+            if (controller.soundEngine.radioSchedule === null) {
+                for (let lilj = 0; lilj < controller.soundEngine.DefaultPlaylist.length; lilj++) {
+                    const item = controller.soundEngine.DefaultPlaylist[lilj];
+                    const option = document.createElement("option");
+                    option.value = item;
+                    controller.newplaylistoptions.appendChild(option);
+                }
+            } else {
+                for (let i = 0; i < controller.soundEngine.radioSchedule.length; i++) {
+                    const item = controller.soundEngine.radioSchedule[i];
+                    switch (subItem) {
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                        case 19:
+                        case 20:
+                        case 21:
+                        case 22:
+                        case 23:
+                            for (let k = 0; k < item.length; k++) {
+                                const subolder = item[k];
+                                for (let l = 0; l < subolder.length; l++) {
+                                    const subItem = subolder[l];
+                                    const option = document.createElement("option");
+                                    option.value = subItem;
+                                    controller.newplaylistoptions.appendChild(option);
+                                }
+                            }
+                            break;
+                        default:
+                            for (let j = 0; j < item.length; j++) {
+                                const subItem = item[j];
+                                const option = document.createElement("option");
+                                option.value = subItem;
+                                controller.newplaylistoptions.appendChild(option);
+                            }
+                            break;
+                    }
+                }
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
 });
--- JS/TFN/maxwell.js
+++ JS/TFN/maxwell.js
@@ -803,61 +803,7 @@ export class maxwell {
 
                     for (const key in sounds) fxSources[key] = this.soundEngine.initAudioContext(this.worker, sounds[key], { type: "video", element: "audio" }, "video");
 
-                    if (this.soundEngine.radioSchedule === null) {
-                        for (let lilj = 0; lilj < this.soundEngine.DefaultPlaylist.length; lilj++) {
-                            const item = this.soundEngine.DefaultPlaylist[lilj];
-                            const option = document.createElement("option");
-                            option.value = item;
-                            this.newplaylistoptions.appendChild(option);
-                        }
-                    } else {
-                        for (let i = 0; i < this.soundEngine.radioSchedule.length; i++) {
-                            const item = this.soundEngine.radioSchedule[i];
-                            switch (subItem) {
-                                case 0:
-                                case 1:
-                                case 2:
-                                case 3:
-                                case 4:
-                                case 5:
-                                case 6:
-                                case 7:
-                                case 8:
-                                case 9:
-                                case 10:
-                                case 12:
-                                case 13:
-                                case 14:
-                                case 15:
-                                case 16:
-                                case 17:
-                                case 18:
-                                case 19:
-                                case 20:
-                                case 21:
-                                case 22:
-                                case 23:
-                                    for (let k = 0; k < item.length; k++) {
-                                        const subolder = item[k];
-                                        for (let l = 0; l < subolder.length; l++) {
-                                            const subItem = subolder[l];
-                                            const option = document.createElement("option");
-                                            option.value = subItem;
-                                            this.newplaylistoptions.appendChild(option);
-                                        }
-                                    }
-                                    break;
-                                default:
-                                    for (let j = 0; j < item.length; j++) {
-                                        const subItem = item[j];
-                                        const option = document.createElement("option");
-                                        option.value = subItem;
-                                        this.newplaylistoptions.appendChild(option);
-                                    }
-                                    break;
-                            }
-                        }
-                    }
+
                     //webcam
                     this.onMe("TfStartShit", "click", async () => {
                         if (!this.videoEngine.webcamstream) {
@@ -924,11 +870,10 @@ export class maxwell {
                             this.soundEngine.loadaudio(URL.createObjectURL(file));
                         }
                     }, false, this.iframe.frame)
-                    /*
-                                        this.onMe("liveplaylist", "change", async (e) => {
-                                            this.soundEngine.loadaudio(URL.createObjectURL(e.target.value));
-                                        }, false, this.iframe.frame)
-                    */
+
+                    this.onMe("liveplaylist", "change", async (e) => {
+                        this.soundEngine.loadaudio(URL.createObjectURL(e.target.value));
+                    }, false, this.iframe.frame)
 
                     this.onMe("TFchromaKey", "click", async () => {
 
